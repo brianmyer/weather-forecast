1c9165469f3e695033b7e23e41478d75c7909919
brianmyer <myer.brian@gmail.com>
2023-08-04 16:43:50 -0400

    able to display times, but the times are incorrect

--- assets/script.js
+++ assets/script.js
@@ -51,17 +51,17 @@ searchBtn.addEventListener('click', function(event) {
 })
 .then(function (firstCity) {
     console.log(firstCity);
-    // convert dt to time format and get icon instead of url to render
-    currentCity.textContent = `${firstCity.city.name} ${firstCity.list[0].dt} https://openweathermap.org/img/wn/${firstCity.list[0].weather[0].icon}@2x.png`
+    // wrong times, get icon instead of url to render
+    currentCity.textContent = `${firstCity.city.name} ${moment(firstCity.list[0].dt).format('MMM DD, YYYY')} https://openweathermap.org/img/wn/${firstCity.list[0].weather[0].icon}@2x.png`
     // convert from K to F
     currentCityTemp.textContent = `Temp: ${firstCity.list[0].main.temp}°F`
     currentCityWind.textContent = `Wind: ${firstCity.list[0].wind.speed} MPH`
     currentCityHumidity.textContent = `Humidity: ${firstCity.list[0].main.humidity}%`
-    day1.textContent = `${firstCity.list[8].dt}`
-    day2.textContent = `${firstCity.list[16].dt}`
-    day3.textContent = `${firstCity.list[24].dt}`
-    day4.textContent = `${firstCity.list[32].dt}`
-    day5.textContent = `${firstCity.list[39].dt}`
+    day1.textContent = `${moment(firstCity.list[8].dt).format('MMM DD, YYYY')}`
+    day2.textContent = `${moment(firstCity.list[16].dt).format('MMM DD, YYYY')}`
+    day3.textContent = `${moment(firstCity.list[24].dt).format('MMM DD, YYYY')}`
+    day4.textContent = `${moment(firstCity.list[32].dt).format('MMM DD, YYYY')}`
+    day5.textContent = `${moment(firstCity.list[39].dt).format('MMM DD, YYYY')}`
     // string literal?
     day1Weather.textContent = `https://openweathermap.org/img/wn/${firstCity.list[8].weather[0].icon}@2x.png
     Temp: ${firstCity.list[8].main.temp}°F 
@@ -83,7 +83,7 @@ searchBtn.addEventListener('click', function(event) {
     Temp: ${firstCity.list[39].main.temp}°F 
     Wind: ${firstCity.list[39].wind.speed} MPH 
     Humidity: ${firstCity.list[39].main.humidity}%`
-
+console.log(`https://openweathermap.org/img/wn/${firstCity.list[8].weather[0].icon}@2x.png`)
     
     
 

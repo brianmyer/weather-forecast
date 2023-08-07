9257ff35e7a54aa1ff0b2557aaedc543e7a5e284
brianmyer <myer.brian@gmail.com>
2023-08-07 15:11:15 -0400

    cleaned up text layout, rendered all icons to appropriate elements

--- assets/script.js
+++ assets/script.js
@@ -10,11 +10,17 @@ let currentCity = document.querySelector('#current-city');
 let currentCityTemp = document.querySelector('#current-city-temp');
 let currentCityWind = document.querySelector('#current-city-wind');
 let currentCityHumidity = document.querySelector('#current-city-humidity');
+let currentCityImg = document.querySelector('#current-city-img')
 let day1 = document.querySelector('#day1');
 let day2 = document.querySelector('#day2');
 let day3 = document.querySelector('#day3');
 let day4 = document.querySelector('#day4');
 let day5 = document.querySelector('#day5');
+let img1 = document.querySelector('#img1');
+let img2 = document.querySelector('#img2');
+let img3 = document.querySelector('#img3');
+let img4 = document.querySelector('#img4');
+let img5 = document.querySelector('#img5');
 let day1Weather = document.querySelector('#day1-weather');
 let day2Weather = document.querySelector('#day2-weather');
 let day3Weather = document.querySelector('#day3-weather');
@@ -60,7 +66,7 @@ function getForecast() {
         let firstCity = citiesFound[0];
         console.log(firstCity.lat);
         console.log(firstCity.lon);
-        return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&appid=${APIKey}`)
+        return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&units=imperial&appid=${APIKey}`)
     })
     
     .then(function (response) {
@@ -69,8 +75,9 @@ function getForecast() {
     .then(function (firstCity) {
         console.log(firstCity);
         // get icon instead of url to render
-        currentCity.textContent = `${firstCity.city.name} ${moment(firstCity.list[0].dt*1000).format('MMM DD, YYYY')} https://openweathermap.org/img/wn/${firstCity.list[0].weather[0].icon}@2x.png`
-        // convert from K to F
+        // target image attribute in card
+        currentCityImg.setAttribute('src', `https://openweathermap.org/img/wn/${firstCity.list[0].weather[0].icon}@2x.png`)
+        currentCity.textContent = `${firstCity.city.name} - ${moment(firstCity.list[0].dt*1000).format('MMM DD, YYYY')}`
         currentCityTemp.textContent = `Temp: ${firstCity.list[0].main.temp}°F`
         currentCityWind.textContent = `Wind: ${firstCity.list[0].wind.speed} MPH`
         currentCityHumidity.textContent = `Humidity: ${firstCity.list[0].main.humidity}%`
@@ -79,26 +86,33 @@ function getForecast() {
         day3.textContent = `${moment(firstCity.list[24].dt*1000).format('MMM DD, YYYY')}`
         day4.textContent = `${moment(firstCity.list[32].dt*1000).format('MMM DD, YYYY')}`
         day5.textContent = `${moment(firstCity.list[39].dt*1000).format('MMM DD, YYYY')}`
+        img1.setAttribute('src', `https://openweathermap.org/img/wn/${firstCity.list[8].weather[0].icon}@2x.png`)
+        img2.setAttribute('src', `https://openweathermap.org/img/wn/${firstCity.list[16].weather[0].icon}@2x.png`)
+        img3.setAttribute('src', `https://openweathermap.org/img/wn/${firstCity.list[24].weather[0].icon}@2x.png`)
+        img4.setAttribute('src', `https://openweathermap.org/img/wn/${firstCity.list[32].weather[0].icon}@2x.png`)
+        img5.setAttribute('src', `https://openweathermap.org/img/wn/${firstCity.list[39].weather[0].icon}@2x.png`)
+
+
         // string literal?
-        day1Weather.textContent = `https://openweathermap.org/img/wn/${firstCity.list[8].weather[0].icon}@2x.png
-        Temp: ${firstCity.list[8].main.temp}°F 
-        Wind: ${firstCity.list[8].wind.speed} MPH 
+        day1Weather.innerHTML = `
+        Temp: ${firstCity.list[8].main.temp}°F <br>
+        Wind: ${firstCity.list[8].wind.speed} MPH <br>
         Humidity: ${firstCity.list[8].main.humidity}%`
-        day2Weather.textContent = `https://openweathermap.org/img/wn/${firstCity.list[16].weather[0].icon}@2x.png
-        Temp: ${firstCity.list[16].main.temp}°F 
-        Wind: ${firstCity.list[16].wind.speed} MPH 
+        day2Weather.innerHTML = `
+        Temp: ${firstCity.list[16].main.temp}°F <br>
+        Wind: ${firstCity.list[16].wind.speed} MPH <br>
         Humidity: ${firstCity.list[16].main.humidity}%`
-        day3Weather.textContent = `https://openweathermap.org/img/wn/${firstCity.list[24].weather[0].icon}@2x.png
-        Temp: ${firstCity.list[24].main.temp}°F 
-        Wind: ${firstCity.list[24].wind.speed} MPH 
+        day3Weather.innerHTML = `
+        Temp: ${firstCity.list[24].main.temp}°F <br>
+        Wind: ${firstCity.list[24].wind.speed} MPH <br>
         Humidity: ${firstCity.list[24].main.humidity}%`
-        day4Weather.textContent = `https://openweathermap.org/img/wn/${firstCity.list[32].weather[0].icon}@2x.png
-        Temp: ${firstCity.list[32].main.temp}°F 
-        Wind: ${firstCity.list[32].wind.speed} MPH 
+        day4Weather.innerHTML = `
+        Temp: ${firstCity.list[32].main.temp}°F <br>
+        Wind: ${firstCity.list[32].wind.speed} MPH <br>
         Humidity: ${firstCity.list[32].main.humidity}%`
-        day5Weather.textContent = `https://openweathermap.org/img/wn/${firstCity.list[39].weather[0].icon}@2x.png
-        Temp: ${firstCity.list[39].main.temp}°F 
-        Wind: ${firstCity.list[39].wind.speed} MPH 
+        day5Weather.innerHTML = `
+        Temp: ${firstCity.list[39].main.temp}°F <br>
+        Wind: ${firstCity.list[39].wind.speed} MPH <br>
         Humidity: ${firstCity.list[39].main.humidity}%`
         
         
